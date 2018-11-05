625778791c46c2f80ea18e31435924ab567c9c14
Andrei Marcu <animar71@gmail.com>
2018-11-05 17:59:56 +0200

    Added watchOS framework

---
+++ ObjectMapper_Realm watchOS/ObjectMapper_Realm_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  ObjectMapper_Realm_watchOS.h
+//  ObjectMapper_Realm watchOS
+//
+//  Created by Andrei Marcu on 05/11/2018.
+//  Copyright © 2018 Jake Peterson. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for ObjectMapper_Realm_watchOS.
+FOUNDATION_EXPORT double ObjectMapper_Realm_watchOSVersionNumber;
+
+//! Project version string for ObjectMapper_Realm_watchOS.
+FOUNDATION_EXPORT const unsigned char ObjectMapper_Realm_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectMapper_Realm_watchOS/PublicHeader.h>
+
+
